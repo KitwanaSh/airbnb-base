6de6c80398e3403419b0385c2a1cb06d8edff6ac
KitwanaSh <rodriguekitweze@gmail.com>
2024-11-27 21:40:59 +0200

    Feat: Add a tempo change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,31 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
-import DestopLogo from "../../public/airbnb-desktop.png";
+import DesktopLogo from "../../public/airbnb-desktop.png";
 import MobileLogo from "../../public/airbnb-mobile.webp";
 import UserNav from "./UserNav";
+import { SearchModalCompnent } from "./SearchComponent";
 
-export default function Navbar() {
-    return (
-        <nav className="w-full border-b">
-            <div className="flex items-center justify-between container mx-auto h-16 px-5 lg:px-10 py-5">
-                <Link href="/">
-                    <Image 
-                        src={DestopLogo}
-                        alt="logo destop"
-                        className="w-32 hidden lg:block"
-                    />
+export function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/">
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-32 hidden lg:block"
+          />
 
-                    <Image 
-                        src={MobileLogo}
-                        alt="logo mobile"
-                        className="block lg:hidden w-12"
-                    />
-                </Link>
-                <div className="border rounded-3xl border-gray-200 px-4 py-2">
-                    <span>Search button</span>
-                </div>
-                <UserNav />
-            </div>
-        </nav>
-    )
+          <Image
+            src={MobileLogo}
+            alt="Mobile Logo"
+            className="block lg:hidden w-12"
+          />
+        </Link>
+
+        <SearchModalCompnent />
+
+        <UserNav />
+      </div>
+    </nav>
+  );
 }
--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -1,30 +1,22 @@
+import { createCategoryPage } from "@/app/actions";
+import CreationBottomBar from "@/app/components/CreationBottomBar";
 import SelectedCategory from "@/app/components/SelectedCategory";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
-export default function StructureRoute() {
-    return (
-        <>
-            <div className="w-3/5 mx-auto">
-                <h2 className="text-3xl font-semibold tracking-tight transition-colors">
-                    Which one of these best describe your place?
-                </h2>
-            </div>
+export default function StrucutreRoute({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
+          Which of these best describe your Home?
+        </h2>
+      </div>
 
-            <form>
-                <SelectedCategory />
+      <form action={createCategoryPage}>
+        <input type="hidden" name="homeId" value={params.id} />
+        <SelectedCategory />
 
-                <div className="fixed w-full bottom-0 z-10 bg-white border-t h-24">
-                    <div className="flex items-center justify-between mx-auto px-5 lg:px-10 h-full">
-                        <Button variant="secondary" size="lg" asChild>
-                            <Link href="/">Cancel</Link>
-                        </Button>
-                        <Button size="lg">
-                            Save
-                        </Button>
-                    </div>
-                </div>
-            </form>
-        </>
-    );
+        <CreationBottomBar />
+      </form>
+    </>
+  );
 }
